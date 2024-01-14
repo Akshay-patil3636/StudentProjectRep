444bdae8b0bdacb7db8ce039236c13f1b79a441c
Akshay-patil3636 <patilakshay9251@gmail.com>
2024-01-14 13:20:59 +0530

    Commited on A branch

---
+++ StudentName/src/studentAddress/AbranchClass.java
@@ -0,0 +1,10 @@
+package studentAddress;
+
+public class AbranchClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("A branch code");
+	}
+
+}
