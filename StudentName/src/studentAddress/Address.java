84262bea30f9df84da3510ebd641a075b311d4ab
Akshay-patil3636 <patilakshay9251@gmail.com>
2024-01-14 13:11:20 +0530

    first commit done

---
+++ StudentName/src/studentAddress/Address.java
@@ -0,0 +1,10 @@
+package studentAddress;
+
+public class Address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome Address");
+	}
+
+}
